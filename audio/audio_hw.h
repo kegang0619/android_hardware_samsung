48b1147833ff6240b779f7f75dd932e2cee60eb2
DarkLord1731 <darklord1731@gmail.com>
2018-04-06 11:16:20 +0530

    derp test

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -20,7 +20,10 @@
 
 #include <cutils/list.h>
 #include <hardware/audio.h>
+
+#if SUPPORTS_AMPLIFIER
 #include <hardware/audio_amplifier.h>
+#endif
 
 #include <tinyalsa/asoundlib.h>
 #include <tinycompress/tinycompress.h>
@@ -430,7 +433,10 @@ struct audio_device {
     int                     (*sound_trigger_close_for_streaming)(int);
 
     pthread_mutex_t         lock_inputs; /* see note below on mutex acquisition order */
+
+#if SUPPORTS_AMPLIFIER
     amplifier_device_t      *amp;
+#endif
 };
 
 /*
